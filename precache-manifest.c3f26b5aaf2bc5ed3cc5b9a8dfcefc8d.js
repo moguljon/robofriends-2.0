d70a542816983c5f65179c8db443d50dce914ebc
Jonathan Aguilar <jonathanaguilar@moguljon.com>
2020-06-06 18:00:11 -0400

    Updates

---
+++ precache-manifest.c3f26b5aaf2bc5ed3cc5b9a8dfcefc8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8760c71738d5aeae6b46e988d049d074",
+    "url": "/robofriends-2.0/index.html"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends-2.0/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4b7b20444cc7e4f6fbc8",
+    "url": "/robofriends-2.0/static/css/main.1700e3cb.chunk.css"
+  },
+  {
+    "revision": "81c60b3770546a60310e",
+    "url": "/robofriends-2.0/static/js/2.ce3203d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends-2.0/static/js/2.ce3203d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b7b20444cc7e4f6fbc8",
+    "url": "/robofriends-2.0/static/js/main.52035740.chunk.js"
+  },
+  {
+    "revision": "25e4ff16827823041832",
+    "url": "/robofriends-2.0/static/js/runtime-main.6cba5ac6.js"
+  }
+]);
